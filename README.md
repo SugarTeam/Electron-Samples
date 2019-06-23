f35a408545e4368aa1a8d808638ce3f1a2baf50d
panxiao <panxiao@cvte.com>
2019-06-23 21:58:42 +0800

    add menu code

--- README.md
+++ README.md
@@ -3,3 +3,16 @@
 在Electron的官网中，列举了大量的API供使用者查阅。但是大部分API的文档很简陋，只有一两句话来描述API如何使用。使得开发过程中需要在各处查找资料和demo。
 
 本项目针对大部分常用API，逐一给出具体的demo供开发者参考。同时在对应的readme中，说明使用方法和注意事项。
+
+## 运行方式
+
+```bash
+npm install electron@4.0.0 -g
+
+cd ./src/Menu
+npm run start
+```
+
+## Electron版本
+
+v4.0.0稳定版
---
+++ src/Menu/main/index.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow, Menu } = require('electron');
+
+let template = [{
+  label: '操作',
+  submenu: [{
+    label: '复制',
+    role: 'copy'
+  },{
+    lable: '剪切',
+    role: 'cut'
+  }]
+}]
+
+const menu = Menu.buildFromTemplate(template);
+
+//通过MenuItem可以动态加减菜单栏
+const menuItem = new MenuItem({
+    label: '最小化',
+    role: 'minimize'
+  });
+  menu.append(menuItem); 
+  
+Menu.setApplicationMenu(menu);
+
+function createWindow () {   
+  // 创建浏览器窗口
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  // 加载index.html文件
+  win.loadFile('./renderer/index.html')
+}
+
+app.on('ready', createWindow);
